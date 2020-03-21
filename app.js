2aa1b4e13c7355f8ee144a69f3ac225901b57d23
Kelton Meinecke <kmeinecke@gmail.com>
2020-03-20 22:51:05 -0600

    dynamic div create is working

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 // ===========================
+//       Variables
+// ===========================
+
+let currentRowID = 1;
+let row = null;
+// ===========================
 //      Query Selectors
 // ===========================
 
 // form query selector
 const addMemeForm = document.querySelector("#form");
+// main div query selector
+const mainDiv = document.querySelector("#main");
 // row div query selector
-const rowDiv = document.querySelector("#row");
+const rowDiv = document.querySelector(".start");
 // URL input query seletor
 let urlInput = document.querySelector("#urlInput");
 // topText input query selector
 let topText = document.querySelector("#topText");
 // bottomeText input query selector
 let bottomText = document.querySelector("#bottomText");
+
+// ===========================
+//        Assignments
+// ===========================
+
+//rowDiv.id = currentRowID;
 
 // ===========================
 //         Listeners
@@ -21,8 +35,29 @@ let bottomText = document.querySelector("#bottomText");
 addMemeForm.addEventListener("submit", function(e) {
   // prevent page from reloading when submitting form
   e.preventDefault();
-  // create meme function call
-  createMeme();
+
+  // create firs row div
+  if (mainDiv.childElementCount === 0) {
+    row = createRowDiv();
+    mainDiv.append(row);
+    console.log("case 0");
+  }
+
+  // create meme
+  if (row.id === currentRowID.toString() && row.childElementCount < 2) {
+    // create meme function call
+    createMeme();
+    console.log("case 1");
+
+    // create new row and meme
+  } else {
+    currentRowID++;
+    row = createRowDiv();
+    mainDiv.append(row);
+    createMeme();
+    console.log("case 2");
+  }
+
   //reset form
   addMemeForm.reset();
   console.log("added image");
@@ -68,7 +103,14 @@ function createMeme() {
   // append bottomDiv to newDiv
   newDiv.append(bottomDiv);
   //append newDiv to mainDiv
-  rowDiv.append(newDiv);
+  row.append(newDiv);
+}
+
+function createRowDiv() {
+  let newRowDiv = createDiv();
+  newRowDiv.classList.add("row");
+  newRowDiv.id = currentRowID;
+  return newRowDiv;
 }
 
 function createDiv() {
